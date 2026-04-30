8ea3062df8b2945e8651e55ea4e6415e3228b02a
Mohammad-Anar <ai.anarul4232@gmail.com>
2026-05-01 03:03:43 +0600

    feat(academy): update routes for formData support

--- src/app/modules/academy/academy.route.ts
+++ src/app/modules/academy/academy.route.ts
@@ -5,6 +5,8 @@ import { AdminRole, UserRole } from "@prisma/client";
 import { AcademyController } from "./academy.controller.js";
 import { AcademyValidation } from "./academy.validation.js";
 
+import fileUploadHandler from "../../middlewares/fileUploadHandler.js";
+
 const router = express.Router();
 
 router.get("/", auth(AdminRole.SUPER_ADMIN, UserRole.PROFESSIONAL_CLUB, UserRole.ACADEMY), AcademyController.getAllAcademies);
@@ -13,6 +15,13 @@ router.get("/:id", auth(AdminRole.SUPER_ADMIN, UserRole.PROFESSIONAL_CLUB, UserR
 router.post(
   "/",
   auth(AdminRole.SUPER_ADMIN, UserRole.PROFESSIONAL_CLUB),
+  fileUploadHandler(),
+  (req, res, next) => {
+    if (req.body.data) {
+      req.body = JSON.parse(req.body.data);
+    }
+    next();
+  },
   validateRequest(AcademyValidation.createAcademyZodSchema),
   AcademyController.createAcademy
 );
@@ -20,6 +29,13 @@ router.post(
 router.patch(
   "/:id",
   auth(AdminRole.SUPER_ADMIN, UserRole.PROFESSIONAL_CLUB, UserRole.ACADEMY),
+  fileUploadHandler(),
+  (req, res, next) => {
+    if (req.body.data) {
+      req.body = JSON.parse(req.body.data);
+    }
+    next();
+  },
   validateRequest(AcademyValidation.updateAcademyZodSchema),
   AcademyController.updateAcademy
 );
