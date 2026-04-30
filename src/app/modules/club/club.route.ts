db753e05da8d1e89e5390cd9d8c24f619fd93734
Mohammad-Anar <ai.anarul4232@gmail.com>
2026-05-01 03:03:29 +0600

    feat(club): update routes for formData support

--- src/app/modules/club/club.route.ts
+++ src/app/modules/club/club.route.ts
@@ -5,6 +5,8 @@ import { AdminRole, UserRole } from "@prisma/client";
 import { ClubController } from "./club.controller.js";
 import { ClubValidation } from "./club.validation.js";
 
+import fileUploadHandler from "../../middlewares/fileUploadHandler.js";
+
 const router = express.Router();
 
 router.get("/", auth(AdminRole.SUPER_ADMIN, UserRole.PROFESSIONAL_CLUB), ClubController.getAllClubs);
@@ -13,6 +15,13 @@ router.get("/:id", auth(AdminRole.SUPER_ADMIN, UserRole.PROFESSIONAL_CLUB, UserR
 router.post(
   "/",
   auth(AdminRole.SUPER_ADMIN),
+  fileUploadHandler(),
+  (req, res, next) => {
+    if (req.body.data) {
+      req.body = JSON.parse(req.body.data);
+    }
+    next();
+  },
   validateRequest(ClubValidation.createClubZodSchema),
   ClubController.createClub
 );
@@ -20,6 +29,13 @@ router.post(
 router.patch(
   "/:id",
   auth(AdminRole.SUPER_ADMIN),
+  fileUploadHandler(),
+  (req, res, next) => {
+    if (req.body.data) {
+      req.body = JSON.parse(req.body.data);
+    }
+    next();
+  },
   validateRequest(ClubValidation.updateClubZodSchema),
   ClubController.updateClub
 );
