e2f90a010f220e6bef072aac5e26e0bbddc64ac5
Mohammad-Anar <ai.anarul4232@gmail.com>
2026-04-30 23:58:13 +0600

    feat(shared): add common response types and register user routes

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,32 +1,12 @@
 import express from "express";
-import { UserRouter } from "../modules/auth/user.route.js";
-import { FighterRouter } from "../modules/fighter/fighter.route.js";
-import { EventRouter } from "../modules/event/event.route.js";
-import { BoutRouter } from "../modules/bout/bout.route.js";
-import { LeagueRouter } from "../modules/league/league.route.js";
-import { TeamRouter } from "../modules/team/team.route.js";
-import { DraftRouter } from "../modules/draft/draft.route.js";
-import { TradeRouter } from "../modules/trade/trade.route.js";
-import { QueueRouter } from "../modules/queue/queue.route.js";
-import { SystemRouter } from "../modules/system/system.route.js";
-import { NewsletterRoutes } from "../modules/newsletter/newsletter.route.js";
-import { DivisionRoutes } from "../modules/division/division.route.js";
+import { AuthRouter } from "../modules/auth/auth.route.js";
+import { UserRoutes } from "../modules/user/user.route.js";
 
 const router = express.Router();
 
 const moduleRoutes = [
-  { path: "/auth",    route: UserRouter },
-  { path: "/fighter", route: FighterRouter },
-  { path: "/event",   route: EventRouter },
-  { path: "/bout",    route: BoutRouter },
-  { path: "/league",  route: LeagueRouter },
-  { path: "/team",    route: TeamRouter },
-  { path: "/draft",   route: DraftRouter },
-  { path: "/trade",   route: TradeRouter },
-  { path: "/queue",   route: QueueRouter },
-  { path: "/system",  route: SystemRouter },
-  { path: "/newsletter", route: NewsletterRoutes },
-  { path: "/division", route: DivisionRoutes },
+  { path: "/auth", route: AuthRouter },
+  { path: "/users", route: UserRoutes },
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
