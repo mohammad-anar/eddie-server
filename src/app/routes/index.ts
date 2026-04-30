8e126c75f1f7f23c9c09d2e7df2b9d9f02f3378f
Mohammad-Anar <ai.anarul4232@gmail.com>
2026-05-01 02:57:48 +0600

    feat(transfer): register transfer routes

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -7,6 +7,13 @@ import { TeamRoutes } from "../modules/team/team.route.js";
 import { CoachRoutes } from "../modules/coach/coach.route.js";
 import { ParentRoutes } from "../modules/parent/parent.route.js";
 import { PlayerRoutes } from "../modules/player/player.route.js";
+import { LeagueRoutes } from "../modules/league/league.route.js";
+import { MatchRoutes } from "../modules/match/match.route.js";
+import { SubscriptionPlanRoutes } from "../modules/subscriptionPlan/subscriptionPlan.route.js";
+import { SponsorRoutes } from "../modules/sponsor/sponsor.route.js";
+import { ProductRoutes } from "../modules/product/product.route.js";
+import { PostRoutes } from "../modules/post/post.route.js";
+import { TransferRoutes } from "../modules/transfer/transfer.route.js";
 
 const router = express.Router();
 
@@ -19,6 +26,13 @@ const moduleRoutes = [
   { path: "/coaches", route: CoachRoutes },
   { path: "/parents", route: ParentRoutes },
   { path: "/players", route: PlayerRoutes },
+  { path: "/leagues", route: LeagueRoutes },
+  { path: "/matches", route: MatchRoutes },
+  { path: "/subscription-plans", route: SubscriptionPlanRoutes },
+  { path: "/sponsors", route: SponsorRoutes },
+  { path: "/products", route: ProductRoutes },
+  { path: "/posts", route: PostRoutes },
+  { path: "/transfers", route: TransferRoutes },
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
